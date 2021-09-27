4923dc0233a5643152a707b52b93f6b517423bdb
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2021-09-27 18:36:55 +0800

    栈 有效括号

---
+++ src/main/java/stack/有效括号/Solution.java
@@ -0,0 +1,40 @@
+package stack.有效括号;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * @Author 喻可
+ * @Date 2021/9/27 18:29
+ */
+class Solution {
+
+    private static final Map<Character, Character> map = new HashMap<Character, Character>() {{
+        put('{', '}');
+        put('[', ']');
+        put('(', ')');
+        put('?', '?');
+    }};
+
+    public boolean isValid(String s) {
+
+        if (s.length() > 0 && !map.containsKey(s.charAt(0))) {
+            return false;
+        }
+
+        LinkedList<Character> stack = new LinkedList<Character>() {{
+            add('?');
+        }};
+
+        for (Character c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.addLast(c);
+            } else if (map.get(stack.removeLast()) != c) {
+                return false;
+            }
+        }
+        return stack.size() == 1;
+    }
+}
+
---
+++ src/main/java/stack/有效括号/Solution1.java
@@ -0,0 +1,35 @@
+package stack.有效括号;
+
+import linkedlist.ListNode;
+
+import java.util.Stack;
+
+
+/**
+ * leetcode 876
+ *
+ * @Author 喻可
+ * @Date 2021/9/26 17:19
+ */
+public class Solution1 {
+
+    public boolean isValid(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.empty() || c != stack.pop()) {
+                return false;
+            }
+
+        }
+        return stack.empty();
+    }
+}
