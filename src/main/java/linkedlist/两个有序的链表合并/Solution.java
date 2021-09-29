01a6ca5c244e66a66991c8384442597e75f8bab6
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2021-09-29 16:33:32 +0800

    构建链表 全部可以调试

--- src/main/java/linkedlist/两个有序的链表合并/Solution.java
+++ src/main/java/linkedlist/两个有序的链表合并/Solution.java
@@ -13,14 +13,14 @@ import java.util.LinkedList;
 public class Solution {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        LinkedList<String> list = new LinkedList<>();
+
         if (l1 == null) {
             return l2;
         } else if (l2 == null) {
             return l1;
         } else if (l1.val < l2.val) {
             //递归是以栈的形式 先进后出
-            // l1.getNext()指针指向很重要
+            // l1.next指针指向很重要
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
